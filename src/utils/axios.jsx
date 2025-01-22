6e4f7919a068b9e48c9a319578c7ed492a749b03
adi522 <adityanagmal7044@gmail.com>
2025-01-22 20:15:16 +0530

    Final commit

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjA5NDM1MjkwMGU0Zjc2ZWJmMWUwNWJkZTAyNWNiZCIsIm5iZiI6MTczNjg1MjkxOS4zNjksInN1YiI6IjY3ODY0NWI3YWJhYmJiYTA0MGJiOWI4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g5Al3-zsQz2mb_npXMhy25oqeR-HVaXRJFDwCOm2rvg',
-    }
-})
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_TOKEN}`,
+    },
+});
 
-export default instance
+export default instance;
